363159bef26c0ba0ab53f014a62011b81c51fd95
sombir5 <sombirverma5@gmail.com>
2022-09-15 13:50:40 +0530

    p-5

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,7 +1,28 @@
 import React from "react";
-
+import github from "../images/github-icon.gif";
+import linked from "../images/linkedin-icon.gif";
+import "../css/footer.css";
 const Footer = () => {
-  return <div id="contacts">Footer</div>;
+  return (
+    <div id="contacts">
+      <div className="social">
+        <div>
+          <a href="https://github.com/sombir5">
+            <div>
+              <img className="icon" src={github} alt="" />
+            </div>
+          </a>
+        </div>
+        <div>
+          <a href="linkedin.com/in/sombir5">
+            <div>
+              <img className="icon" src={linked} alt="" />
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -14,19 +14,39 @@ const Introduction = () => {
           <h3>Hi, my name is</h3>
           <h1>Sombir</h1>
           <p>
-            I am a Full-stack developer with good knowledge of JavaScript,
-             HTML, CSS, React, MERN and a 1000 hours of coding practice.
-            Looking at joining a challenging work environment where
-             I can develop a deeper understanding of web development.
+            I am a Full-stack developer with good knowledge of JavaScript, HTML,
+            CSS, React, MERN and a 1000 hours of coding practice. Looking at
+            joining a challenging work environment where I can develop a deeper
+            understanding of web development.
           </p>
           <div className="download">
             <a href={Resume} target="_blank">
-              CV
+              <div>CV</div>
             </a>
           </div>
         </div>
         <div className="pic">
           <img className="image" src={sombir} alt="sombir" />
+          <div className="stats">
+            <a
+              href="https://github.com/sombir5?tab=repositories"
+              title="Profile"
+            >
+              <img
+                src="https://github-readme-stats.vercel.app/api?username=sombir5&show_icons=true&custom_title=My%20GitHub%20Stats&border_radius=0"
+                alt="Github Stats"
+              />
+            </a>
+            <a
+              href="https://github.com/sombir5?tab=repositories"
+              title="Profile"
+            >
+              <img
+                src="https://github-readme-streak-stats.herokuapp.com/?user=sombir5&border_radius=0"
+                alt="sombir5"
+              />
+            </a>
+          </div>
         </div>
       </div>
       <Skills />
